dbf7bf11169ee666f171ef816fef10232c6d92f6
Arturas Slajus <x11@arturaz.net>
2014-10-14 17:38:22 +0300

    protobuf generators: support running from any directory

--- protos/gen_client.sh
+++ protos/gen_client.sh
@@ -7,11 +7,13 @@ if [ "$client_dir" == "" ]; then
   exit 1
 fi
 
-targetdir="$client_dir/unity/Assets/gen/netmsg/"
+targetdir="$client_dir/code/game-general/Networking/Messages/"
+targetdir="$(echo $(cd $(dirname $targetdir); pwd)/$(basename $targetdir))"
 thisdir="$(dirname $0)"
 cwd="$(pwd)"
 
 cd "$thisdir"
+echo "Exporting to $targetdir"
 mkdir -p "$targetdir"
-tools/ProtoGen.exe *.proto -namespace=netmsg -output_directory="$targetdir" -nest_classes=true
+tools/ProtoGen.exe *.proto -namespace="game_general.Networking.Messages" -output_directory="$targetdir" -nest_classes=true
 cd "$cwd"
--- protos/gen_server.sh
+++ protos/gen_server.sh
@@ -3,6 +3,10 @@
 thisdir="$(dirname $0)"
 cwd="$(pwd)"
 
+targetdir="$thisdir/../src/gen/java/"
+targetdir="$(echo $(cd $(dirname $targetdir); pwd)/$(basename $targetdir))"
+
 cd "$thisdir"
-tools/protoc.exe *.proto --java_out=../src/gen/java/
+echo "Exporting to $targetdir"
+tools/protoc.exe *.proto "--java_out=$targetdir"
 cd "$cwd"
