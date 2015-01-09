e88b0dc4e6f7ba4d7d1385f9d26015b301931259
Arturas Slajus <arturas@tinylabproductions.com>
2015-01-09 16:10:27 +0200

    Support game per user, clean up games map after game is finished.

--- src/main/scala/app/actors/game/GamesManagerActor.scala
+++ src/main/scala/app/actors/game/GamesManagerActor.scala
@@ -1,13 +1,14 @@
 package app.actors.game
 
 import akka.actor.SupervisorStrategy.Stop
-import akka.actor.{OneForOneStrategy,  Actor, ActorRef}
+import akka.actor.{Terminated, OneForOneStrategy, Actor, ActorRef}
 import akka.event.LoggingReceive
 import app.models.User
 import implicits._
 
 class GamesManagerActor extends Actor {
-  private[this] var game = Option.empty[ActorRef]
+  private[this] var user2game = Map.empty[User, ActorRef]
+  private[this] var game2user = Map.empty[ActorRef, User]
 
   override def supervisorStrategy = OneForOneStrategy() {
     case _ => Stop
@@ -15,12 +16,19 @@ class GamesManagerActor extends Actor {
 
   override def receive = LoggingReceive {
     case msg @ GameActor.In.Join(user) =>
-      game.fold2(createGame(user, sender()), _.tell(msg, sender()))
+      user2game.get(user).fold2(createGame(user, sender()), _.tell(msg, sender()))
+    case Terminated(ref) =>
+      game2user.get(ref).foreach { user =>
+        game2user -= ref
+        user2game -= user
+      }
   }
 
   private[this] def createGame(user: User, client: ActorRef): ActorRef = {
-    val gameRef = context.actorOf(GameActor.props(user, client))
-    game = Some(gameRef)
-    gameRef
+    val game = context.actorOf(GameActor.props(user, client))
+    context.watch(game)
+    user2game += user -> game
+    game2user += game -> user
+    game
   }
 }
