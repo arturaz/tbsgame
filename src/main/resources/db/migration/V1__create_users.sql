aa1b2b1cf960cbac4bf6d580125bb93ce0146515
Arturas Slajus <arturas@tinylabproductions.com>
2015-01-26 17:50:03 +0200

    Syntax error sql.

--- src/main/resources/db/migration/V1__create_users.sql
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -1,7 +1,7 @@
 create table "users" (
   "id" BLOB(16) PRIMARY KEY NOT NULL,
   "name" VARCHAR(254) NOT NULL,
-  "email" VARCHAR(254) NULL,
+  "email" VARCHAR(254),
   "password" VARCHAR(254) NOT NULL,
   "session_token" VARCHAR(254) NOT NULL
 );
